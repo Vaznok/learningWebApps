beed72ab518974acef377823eacb7e9005d620eb
Vetall <vlasenko.vitaliy.l@gmail.com>
2017-09-12 17:56:12 +0300

    JDBC learning

---
+++ src/main/java/com/vetallWebapp/eshop/dao/demo/example.sql
@@ -0,0 +1,4 @@
+INSERT INTO vetall_list.books (id, `name`, author)
+    VALUES (1, 'Effective Java', 'Joshua Bloch');
+INSERT INTO vetall_list.books (id, `name`, author)
+    VALUES (2, 'Java Concurrency in Practice', 'Brian Goetz');
